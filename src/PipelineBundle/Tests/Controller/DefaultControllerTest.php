ddc16d83c26dd5f3fa8eee6e4edfc5d138782e86
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2015-06-04 11:42:47 -0700

    Fixes up tests more.

--- src/PipelineBundle/Tests/Controller/DefaultControllerTest.php
+++ src/PipelineBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,95 +6,114 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testUp() {
-      $client = static::createClient();
-      $client->request('GET', '/tracker/bananas/3/up');
+  protected function setUp()
+  {
+    self::bootKernel();
+    # TODO this is pretty ugly, but given that there doesn't seem to be a
+    # trivial way to enable transactional tests, we'll live with this for now.
+    $bananas = static::$kernel->getContainer()
+        ->get('doctrine')
+        ->getRepository('PipelineBundle:Thing')
+        ->findOneByName("bananas");
 
-      # when no count, default to zero
-      $this->assertEquals(
-        json_decode($client->getResponse()->getContent(), true),
-        array(
-          "success" => true,
-          "name" => "bananas",
-          "count" => 3,
-        )
-      );
-
-      $client = static::createClient();
-      $client->request('GET', '/tracker/bananas/2/up');
-
-      # when already have a count, modify it.
-      $this->assertEquals(
-        json_decode($client->getResponse()->getContent(), true),
-        array(
-          "success" => true,
-          "name" => "bananas",
-          "count" => 5,
-        )
-      );
+    if (!!$bananas) {
+      $em = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+      $em->remove($bananas);
+      $em->flush();
     }
+  }
 
-    public function testDown() {
-      $client = static::createClient();
-      $client->request('GET', '/tracker/bananas/3/down');
+  public function testUp() {
+    $client = static::createClient();
+    $client->request('GET', '/tracker/bananas/3/up');
 
-      # when no count, default to zero
-      $this->assertEquals(
-        json_decode($client->getResponse()->getContent(), true),
-        array(
-          "success" => true,
-          "name" => "bananas",
-          "count" => -3,
-        )
-      );
+    # when no count, default to zero
+    $this->assertEquals(
+      json_decode($client->getResponse()->getContent(), true),
+      array(
+        "success" => true,
+        "name" => "bananas",
+        "count" => 3,
+      )
+    );
 
-      $client = static::createClient();
-      $client->request('GET', '/tracker/bananas/2/down');
+    $client = static::createClient();
+    $client->request('GET', '/tracker/bananas/2/up');
 
-      # when already have a count, modify it.
-      $this->assertEquals(
-        json_decode($client->getResponse()->getContent(), true),
-        array(
-          "success" => true,
-          "name" => "bananas",
-          "count" => -5,
-        )
-      );
-    }
+    # when already have a count, modify it.
+    $this->assertEquals(
+      json_decode($client->getResponse()->getContent(), true),
+      array(
+        "success" => true,
+        "name" => "bananas",
+        "count" => 5,
+      )
+    );
+  }
 
-    public function testInvalidDirection() {
-      $client = static::createClient();
-      $client->request('GET', '/tracker/bananas/3/foo');
+  public function testDown() {
+    $client = static::createClient();
+    $client->request('GET', '/tracker/bananas/3/down');
 
-      $this->assertEquals(
-        json_decode($client->getResponse()->getContent(), true),
-        array(
-          "success" => false,
-          "error" => "invalid direction foo",
-        )
-      );
-    }
+    # when no count, default to zero
+    $this->assertEquals(
+      json_decode($client->getResponse()->getContent(), true),
+      array(
+        "success" => true,
+        "name" => "bananas",
+        "count" => -3,
+      )
+    );
 
-    public function testInvalidCount() {
-      $client = static::createClient();
-      $client->request('GET', '/app/tracker/bananas/bar/up');
+    $client = static::createClient();
+    $client->request('GET', '/tracker/bananas/2/down');
 
-      $this->assertEquals(
-        json_decode($client->getResponse()->getContent(), true),
-        array(
-          "success" => false,
-          "error" => "invalid count bar",
-        )
-      );
-    }
+    # when already have a count, modify it.
+    $this->assertEquals(
+      json_decode($client->getResponse()->getContent(), true),
+      array(
+        "success" => true,
+        "name" => "bananas",
+        "count" => -5,
+      )
+    );
+  }
 
-    public function testEvents() {
-      # TODO
-    }
+  public function testInvalidDirection() {
+    $client = static::createClient();
+    $client->request('GET', '/tracker/bananas/3/foo');
 
-    # TODO:
-    #   Test goofy encodings
-    #   Test long string names
-    #   Test large counts
-    #   Test negative counts
+    $this->assertEquals(
+      json_decode($client->getResponse()->getContent(), true),
+      array(
+        "success" => false,
+        "error" => "invalid direction foo",
+      )
+    );
+  }
+
+  public function testInvalidCount() {
+    $client = static::createClient();
+    $client->request('GET', '/app/tracker/bananas/bar/up');
+
+    $this->assertEquals(
+      json_decode($client->getResponse()->getContent(), true),
+      array(
+        "success" => false,
+        "error" => "invalid count bar",
+      )
+    );
+  }
+
+  public function testEvents() {
+    # TODO
+  }
+
+  # TODO:
+  #   Test goofy encodings
+  #   Test long string names
+  #   Test large counts
+  #   Test negative counts
 }
