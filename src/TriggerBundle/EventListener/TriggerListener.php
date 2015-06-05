52bb384362df3f38780a5958bff5fcaf4ede4190
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2015-06-05 00:21:27 -0700

    Adds an actual event listener

---
+++ src/TriggerBundle/EventListener/TriggerListener.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TriggerBundle\EventListener;
+
+use PipelineBundle\PipelineUpdateEvent;
+
+class TriggerListener
+{
+  public function checkBananasCount(PipelineUpdateEvent $event) {
+  }
+
+  public function checkMessagesCount(PipelineUpdateEvent $event) {
+  }
+}
